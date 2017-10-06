e4cf62f1091ca5c837cb9c9668a0d9e218621ffc
Rishabh Singh <pingrishabh@gmail.com>
2017-10-06 19:49:40 +0530

    Fourth Commit
    Added a New Activity For URL Analysis Purposes. Added:
    
    1. URL Field
    
    2. Submit Button
    
    3. Host Detection

--- App/app/src/main/java/com/srmhackathon/credible/MainActivity.java
+++ App/app/src/main/java/com/srmhackathon/credible/MainActivity.java
@@ -2,12 +2,59 @@ package com.srmhackathon.credible;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText url;
+
+    private String urlcheck;
+
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        url = (EditText) findViewById(R.id.url);
+
+        submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(this);
+
     }
+
+
+    private void alert(String message) {
+
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+
+    }
+
+
+    private void hostDetection() {
+
+        urlcheck = url.getText().toString().trim().substring(0, urlcheck.indexOf(".com"));
+
+        alert(urlcheck);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == submit) {
+
+            hostDetection();
+            
+        }
+
+    }
+
+
 }
