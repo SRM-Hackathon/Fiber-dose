a3f5af1d92c624493c0d762eb9093ca1ac1c84cc
Rishabh Singh <pingrishabh@gmail.com>
2017-10-07 04:23:27 +0530

    Ninth Commit
     Retreiving Negetive Or Positive Context From The Post Title

--- App/app/src/main/java/com/srmhackathon/credible/MainActivity.java
+++ App/app/src/main/java/com/srmhackathon/credible/MainActivity.java
@@ -12,11 +12,14 @@ import android.widget.Toast;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EmotionOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.KeywordsOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.SentimentOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -38,13 +41,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private String host;
 
+    private String textscore;
+
     private String scoopwhoop_title;
 
     private String scoopwhoop_content;
 
+    private String USERNAME = "6a872cfb-9761-41ce-a308-271a80101b0a";
+
+    private String PASSWORD = "VF1nPTAeKpNt";
+
     private String SCOOPWHOOP_TITLE = "artTitle";
 
-    private String SCOOPWHOOP_CONTENT = "sw_para";
+    private String SCOOPWHOOP_CONTENT = "articleContentData";
 
     private Button submit;
 
@@ -69,33 +78,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void alert(String message) {
 
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-
-    }
-
-
-    private void watsonNLU(String text) {
-
-        NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27, "6a872cfb-9761-41ce-a308-271a80101b0a", "VF1nPTAeKpNt");
-
-
-        SentimentOptions sentimentOptions = new SentimentOptions.Builder()
-                .document(true)
-                .build();
-
-        Features features = new Features.Builder()
-                .sentiment(sentimentOptions)
-                .build();
-
-        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
-                .text(text)
-                .features(features)
-                .build();
-
-        AnalysisResults response = service
-                .analyze(parameters)
-                .execute();
-
-        Log.d("JSON",response.toString());
 
     }
 
@@ -155,7 +137,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Elements title = doc.select("h1." + SCOOPWHOOP_TITLE);
 
-                Elements body = doc.select("h1." + SCOOPWHOOP_CONTENT).select("p");
+                Elements body = doc.select("div." + SCOOPWHOOP_CONTENT).select("p");
 
                 for (Element element : title) {
 
@@ -169,13 +151,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 }
 
-                watsonNLU(scoopwhoop_title);
+                watsonNLU(scoopwhoop_content, "R1");
 
             } catch (IOException io) {
 
                 alert(io.toString());
 
             }
+
+        }
+
+    }
+
+
+    private void watsonNLU(String text, String context) {
+
+        NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27, USERNAME, PASSWORD);
+
+        SentimentOptions sentimentOptions = new SentimentOptions.Builder()
+                .document(true)
+                .build();
+
+        EmotionOptions emotionOptions = new EmotionOptions.Builder()
+                .document(true)
+                .build();
+
+        Features features = new Features.Builder()
+                .sentiment(sentimentOptions)
+                .emotion(emotionOptions)
+                .build();
+
+        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                .text(text)
+                .features(features)
+                .build();
+
+        AnalysisResults response = service
+                .analyze(parameters)
+                .execute();
+
+        Log.d("NLU", response.toString());
+
+        if (context == "R1") {
+
+            try {
+
+                JSONObject result = new JSONObject(response.toString());
+
+                JSONObject sentiment = result.getJSONObject("sentiment");
+
+                JSONObject document = sentiment.getJSONObject("document");
+
+                textscore = document.getString("score");
+
+                alert(textscore);
+
+            }
+
+            catch (JSONException je) {
+
+                alert(je.toString());
+
+            }
+
+        }
+
+        else {
+
+            alert("Invalid Context");
 
         }
 
